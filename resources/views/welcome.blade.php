dc832c21b7008cf999d39ddb407692ae114ff527
nesh <kulplex@gmail.com>
2018-04-11 23:52:10 +0530

    updated map with location marker and pretty pins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,33 @@
 <html>
   <head>
     <style>
-       #map {
-        height: 400px;
-        width: 100%;
-       }
+        html { height: 100% }
+        body { height: 100%; margin: 0px; padding: 0px }
+        #map { height: 100% }
     </style>
   </head>
   <body>
-    <h3>My Google Maps Demo</h3>
+    <h3>WiFi Speeds around the World</h3>
     <div id="map"></div>
 
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script>
 
+       var connectionRanges = [10, 100, 1000, 10000, Number.MAX_SAFE_INTEGER];
+       var connectionColors = [ "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
+                                "https://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+                                "https://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+                                "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                                "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+                                ];
+
+     function getColorFromSpeed(downloadSpeed) {
+        for(var i = 0; i < connectionRanges.length; i++) {
+            if(downloadSpeed <= connectionRanges[i]) {
+                return connectionColors[i];
+            }
+        }
+     }
 
       function initMap() {
         $.ajax({
@@ -38,7 +52,11 @@
                     '</div>'+
                     '<h1 id="firstHeading" class="firstHeading">' + data[i]["name"] +'</h1>'+
                     '<div id="bodyContent">'+
-                    '<p>' + data[i]["comments"] +'</p>'+
+                    '<p><b>Comments:</b> ' + data[i]["comments"] +'</p>'+
+                    '<p><b>Download:</b> ' + data[i]["download"]/1000 +' Kb/s</p>'+
+                    '<p><b>Upload:</b> ' + data[i]["upload"]/1000 +' Kb/s</p>'+
+                    '<p><b>Ping:</b> ' + data[i]["ping"] +' ms</p>'+
+                    '<p><b>Packet Loss:</b> ' + data[i]["packet_loss"] +'%</p>'+
                     '</div>'+
                     '</div>';
 
@@ -47,9 +65,11 @@
                     });
 
                     marker = new google.maps.Marker({
+                        icon: getColorFromSpeed(data[i]["download"]/1000),
                         position: {lat: data[i]["latitude"], lng: data[i]["longitude"]},
                         map: map,
-                        title: data[i]["name"]
+                        title: data[i]["name"],
+                        animation: google.maps.Animation.DROP
                     });
 
                     google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
@@ -60,6 +80,21 @@
                     })(marker,content,infowindow));
                 }
             }
+            
+            if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+              });
+            }
+
           }
         });
 
